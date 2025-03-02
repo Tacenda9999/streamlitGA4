e2d2592f59891ecb2c86ca480e62c77968f8b8e6
tacenda9999 <tacenda9999@gmail.com>
2025-03-02 23:51:04 +0900

    merge resolve

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import streamlit as st
 
 st.set_page_config(
@@ -7,14 +6,4 @@ st.set_page_config(
 )
 
 st.title('GA4 Test')
-=======
-import streamlit as st
-
-st.set_page_config(
-    page_title="GA4 Test",
-    initial_sidebar_state="expanded",
-)
-
-st.title('GA4 Test')
->>>>>>> 85f59c480d3b73d6dc12c961ddacd4c2d4c37a16
 st.markdown('GA4 Test')
